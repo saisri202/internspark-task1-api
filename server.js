2807da62b7e07a89a08e31daa603b727a2b0280d
VishalaPriya <vishalapriya616@gmail.com>
2026-06-10 17:39:03 +0530

    Completed InternSpark Task 1 CRUD API

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+const productRoutes = require("./routes/productRoutes");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("InternSpark Task 1 API is running 🚀");
+});
+
+app.use("/api/products", productRoutes);
+
+// Error handler should be LAST
+app.use(errorHandler);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed:", error.message);
+  });
